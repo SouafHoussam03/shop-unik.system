8a4c7370cec09ba645c29dc784129200ece58f7c
SouafHoussam03 <115432208+SouafHoussam03@users.noreply.github.com>
2026-06-16 14:56:21 +0100

    Fix login cookie

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -1,65 +1,64 @@
-const bcrypt = require('bcryptjs')
-const userModel = require('../../models/userModel')
+const bcrypt = require('bcryptjs');
+const userModel = require('../../models/userModel');
 const jwt = require('jsonwebtoken');
 
-async function userSignInController(req,res){
-    try{
-        const { email , password} = req.body
+async function userSignInController(req, res) {
+    try {
+        const { email, password } = req.body;
 
-        if(!email){
-            throw new Error("Please provide email")
-        }
-        if(!password){
-             throw new Error("Please provide password")
+        if (!email) {
+            throw new Error("Please provide email");
         }
 
-        const user = await userModel.findOne({email})
+        if (!password) {
+            throw new Error("Please provide password");
+        }
 
-       if(!user){
-            throw new Error("User not found")
-       }
+        const user = await userModel.findOne({ email });
 
-       const checkPassword = await bcrypt.compare(password,user.password)
+        if (!user) {
+            throw new Error("User not found");
+        }
 
-       console.log("checkPassoword",checkPassword)
+        const checkPassword = await bcrypt.compare(password, user.password);
 
-       if(checkPassword){
+        if (!checkPassword) {
+            throw new Error("Please check Password");
+        }
+
         const tokenData = {
-            _id : user._id,
-            email : user.email,
-        }
-        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
+            _id: user._id,
+            email: user.email
+        };
+
+        const token = jwt.sign(
+            tokenData,
+            process.env.TOKEN_SECRET_KEY,
+            { expiresIn: "8h" }
+        );
 
         const tokenOption = {
-            httpOnly : true,
-            secure : true
-        }
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 1000 * 60 * 60 * 8
+        };
 
-        res.cookie("token",token,tokenOption).status(200).json({
-            message : "Login successfully",
-            data : token,
-            success : true,
-            error : false
-        })
+        res.cookie("token", token, tokenOption);
 
-       }else{
-         throw new Error("Please check Password")
-       }
+        return res.status(200).json({
+            message: "Login successfully",
+            success: true,
+            error: false
+        });
 
-
-
-
-
-
-
-    }catch(err){
-        res.json({
-            message : err.message || err  ,
-            error : true,
-            success : false,
-        })
+    } catch (err) {
+        return res.status(400).json({
+            message: err.message || "Login failed",
+            error: true,
+            success: false
+        });
     }
-
 }
 
-module.exports = userSignInController
+module.exports = userSignInController;
